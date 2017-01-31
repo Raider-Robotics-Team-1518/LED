136b3bbfc0870f7cb2942c59ed68247cc9db3858
Tim Poulsen <tim@skypanther.com>
2017-01-30 19:42:28 -0500

    add rainbow palette to solid_color sketch

--- solid_color/solid_color.ino
+++ solid_color/solid_color.ino
@@ -1,9 +1,10 @@
+
 #include "FastLED.h"
 
 int inputPin3 = 3;
 int inputPin4 = 4;
 
-int WHITE = 0;
+int BLACK = 0;
 int BLUE = 1;
 int RED = 2;
 
@@ -22,33 +23,57 @@ int RED = 2;
 // This is an array of leds.  One item for each led in your strip.
 CRGB leds[NUM_LEDS];
 
+#define BRIGHTNESS  150
+#define LED_TYPE    WS2812B // WS2811 also works
+#define COLOR_ORDER GRB
+#define UPDATES_PER_SECOND 250
+
+CRGBPalette16 currentPalette;
+TBlendType    currentBlending;
+
+extern CRGBPalette16 myRedWhiteBluePalette;
+extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
+
+
 // This function sets up the ledsand tells the controller about them
 void setup() {
-  // sanity check delay - allows reprogramming if accidently blowing power w/leds
-  delay(2000);
+  delay(2000); // power-up safety delay
 
-  // FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
-  FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(  BRIGHTNESS );
   pinMode(inputPin3, INPUT);
   pinMode(inputPin4, INPUT);
   digitalWrite(inputPin3, HIGH);
   digitalWrite(inputPin4, HIGH);
 
+  currentPalette = RainbowColors_p;
+  currentBlending = LINEARBLEND;
+
 }
 
 void loop() {
+    ChangePalettePeriodically();
+    
+    static uint8_t startIndex = 0;
+    startIndex = startIndex + 1; /* motion speed */
   int reading3 = 1 - digitalRead(inputPin3);
   int reading4 = 1 - digitalRead(inputPin4);
-  if (reading3 == 1 && reading4 == 1) {
-    turnColor(WHITE);
+  if (reading3 == 0 && reading4 == 0) {
+    
+    FillLEDsFromPaletteColors( startIndex);
+    FastLED.show();
+
+    delay(1000 / UPDATES_PER_SECOND);
+
   } else if (reading3 == 1 && reading4 == 0) {
     turnColor(RED);
+    delay(1000);
   } else if (reading3 == 0 && reading4 == 1) {
     turnColor(BLUE);
+    delay(1000 / UPDATES_PER_SECOND);
   } else {
+    turnColor(BLACK);
   }
-  delay(1000);
 
 }
 
@@ -57,11 +82,118 @@ void turnColor(int color) {
     if (color == BLUE) {
       leds[led] = CRGB::Blue;
     } else if (color == RED) {
-      leds[led] = CRGB::Green;
+      leds[led] = CRGB::Red;
     } else {
-      leds[led] = CRGB::White;
+      leds[led] = CRGB::Black;
     }
     FastLED.show();
   }
 }
+
+
+void FillLEDsFromPaletteColors( uint8_t colorIndex)
+{
+    uint8_t brightness = 255;
+    
+    for( int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
+        colorIndex += 3;
+    }
+}
+
+
+// There are several different palettes of colors demonstrated here.
+//
+// FastLED provides several 'preset' palettes: RainbowColors_p, RainbowStripeColors_p,
+// OceanColors_p, CloudColors_p, LavaColors_p, ForestColors_p, and PartyColors_p.
+//
+// Additionally, you can manually define your own color palettes, or you can write
+// code that creates color palettes on the fly.  All are shown here.
+
+void ChangePalettePeriodically()
+{
+    uint8_t secondHand = (millis() / 1000) % 60;
+    static uint8_t lastSecond = 99;
+    
+    if( lastSecond != secondHand) {
+        lastSecond = secondHand;
+        if( secondHand ==  0)  { currentPalette = RainbowColors_p;         currentBlending = LINEARBLEND; }
+        if( secondHand == 10)  { currentPalette = RainbowStripeColors_p;   currentBlending = NOBLEND;  }
+        if( secondHand == 15)  { currentPalette = RainbowStripeColors_p;   currentBlending = LINEARBLEND; }
+        if( secondHand == 20)  { SetupPurpleAndGreenPalette();             currentBlending = LINEARBLEND; }
+        if( secondHand == 25)  { SetupTotallyRandomPalette();              currentBlending = LINEARBLEND; }
+        if( secondHand == 30)  { SetupBlackAndWhiteStripedPalette();       currentBlending = NOBLEND; }
+        if( secondHand == 35)  { SetupBlackAndWhiteStripedPalette();       currentBlending = LINEARBLEND; }
+        if( secondHand == 40)  { currentPalette = CloudColors_p;           currentBlending = LINEARBLEND; }
+        if( secondHand == 45)  { currentPalette = PartyColors_p;           currentBlending = LINEARBLEND; }
+        if( secondHand == 50)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = NOBLEND;  }
+        if( secondHand == 55)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = LINEARBLEND; }
+    }
+}
+
+// This function fills the palette with totally random colors.
+void SetupTotallyRandomPalette()
+{
+    for( int i = 0; i < 16; i++) {
+        currentPalette[i] = CHSV( random8(), 255, random8());
+    }
+}
+
+// This function sets up a palette of black and white stripes,
+// using code.  Since the palette is effectively an array of
+// sixteen CRGB colors, the various fill_* functions can be used
+// to set them up.
+void SetupBlackAndWhiteStripedPalette()
+{
+    // 'black out' all 16 palette entries...
+    fill_solid( currentPalette, 16, CRGB::Black);
+    // and set every fourth one to white.
+    currentPalette[0] = CRGB::White;
+    currentPalette[4] = CRGB::White;
+    currentPalette[8] = CRGB::White;
+    currentPalette[12] = CRGB::White;
+    
+}
+
+// This function sets up a palette of purple and green stripes.
+void SetupPurpleAndGreenPalette()
+{
+    CRGB purple = CHSV( HUE_PURPLE, 255, 255);
+    CRGB green  = CHSV( HUE_GREEN, 255, 255);
+    CRGB black  = CRGB::Black;
+    
+    currentPalette = CRGBPalette16(
+                                   green,  green,  black,  black,
+                                   purple, purple, black,  black,
+                                   green,  green,  black,  black,
+                                   purple, purple, black,  black );
+}
+
+
+// This example shows how to set up a static color palette
+// which is stored in PROGMEM (flash), which is almost always more
+// plentiful than RAM.  A static PROGMEM palette like this
+// takes up 64 bytes of flash.
+const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM =
+{
+    CRGB::Red,
+    CRGB::Gray, // 'white' is too bright compared to red and blue
+    CRGB::Blue,
+    CRGB::Black,
+    
+    CRGB::Red,
+    CRGB::Gray,
+    CRGB::Blue,
+    CRGB::Black,
+    
+    CRGB::Red,
+    CRGB::Red,
+    CRGB::Gray,
+    CRGB::Gray,
+    CRGB::Blue,
+    CRGB::Blue,
+    CRGB::Black,
+    CRGB::Black
+};
+
 
